65aa1c9e604502797d4e1c63f4fd188894d7d0da
Amia Defreitas <amia.defreitas0@gmail.com>
2019-12-30 14:31:56 -0500

    seeded data

--- bin/run.rb
+++ bin/run.rb
@@ -3,15 +3,36 @@ require_relative '../config/environment'
 
 def greeting
     puts "“Welcome to Adventure of ruby… would you like to play?” y/n?"
-    gets.chomp
-    if "y"
-
-    if "n"
+    user_input = gets.chomp
+    if user_input == "y"
+        puts "Welcome to Adventure of ruby…"
+        
+    elsif user_input == "n"
         puts "GoodBye loser"
-        break
+    else
+        puts "invalid command."
     end
-elsif
-    puts "invalid command type help for other commands"
 end
+
+    def choose_character
+        puts "Choose Your Character.."
+        user_input = gets.chomp
+        if user_input == "1"
+            puts "You Have Chosen Character 1"
+        else
+            puts "Invalid Command"
+        end
+
+    end
+
+def run
+    greeting
+    choose_character
 end
+
+run
+# elsif
+#     puts "invalid command type help for other commands"
+# end
+# end
 # puts "HELLO WORLD"
