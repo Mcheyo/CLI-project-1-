2e0497ac6a508d4d2287881e8fafa06c3377ce0b
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2019-12-31 12:09:20 -0500

    Finished Level2 and heal count

---
+++ bin/level2.rb
@@ -0,0 +1,78 @@
+def level2
+    level_array =[]
+    level_array << Level.all
+    puts "You Have Entered Phase 2 #{level_array[0][1].description}"
+    # fight 
+    # binding.pry
+    
+
+    # name an attack(string) whenever that string is selected 
+    @current_character.health = 100 
+    characters = Character.all
+    enemies = Enemy.all
+    current_enemy = enemies[0]
+    heal_count = 0 
+    puts "1.Attack, 2. Dodge, 3. Heal"
+    
+    # does a reduction on the enemy
+    while @current_character.health > 0 && current_enemy.health > 0
+        user_input = gets.chomp
+        
+        
+    
+    
+        if user_input == "1"
+            puts "#{@current_character.name} attacks #{current_enemy.name}"
+            # binding.pry
+            current_enemy.health -=  rand(1..10)
+            puts "#{current_enemy.name} health is at #{current_enemy.health}."
+
+            puts "#{current_enemy.name} attacks #{@current_character.name} back"
+            @current_character.health -= rand(3..15)
+            puts "Your health is at #{@current_character.health}."
+            puts "1.Attack, 2. Dodge, 3. Heal"
+        elsif user_input == "2"
+            
+            dodge_chance = rand(1..10)
+            if dodge_chance >= 5 
+            puts "#{current_enemy.name} attacks #{@current_character.name} but missed."
+            
+            else
+                puts "#{@current_character.name} attempted to dodge but got hit."
+                @current_character.health -= 7
+                puts "Your health is at #{@current_character.health}."
+
+            end
+            puts "1.Attack, 2. Dodge, 3. Heal"
+        elsif user_input == "3"
+            
+            heal_count +=1 
+           if  heal_count <=3 
+            if @current_character.health > 50 
+                puts "Heal Unavailable..."
+            else
+                puts "You have healed.."
+                @current_character.health += rand(10..45)
+                puts "Your health is at #{@current_character.health}."
+            end
+           elsif heal_count >3
+            puts "You are out of heals"
+        
+           end 
+            puts "1.Attack, 2. Dodge, 3. Heal"
+        else
+            puts "Invlaid command please choose between '1', '2', or '3'" 
+            puts "1.Attack, 2. Dodge, 3. Heal" 
+        end
+        
+    
+    
+    end
+    
+    if current_enemy.health <= 0
+        puts "Congrats you beat #{current_enemy.name} Move on to next level."
+    elsif @current_character.health <=0
+        puts "#{current_enemy.name} proved too much for you...try again another day"
+    end 
+        
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,10 @@
 require_relative '../config/environment'
 require 'pry'
+require_relative '../bin/level1'
+require_relative '../bin/level2'
 
 @current_character = "test"
+@life_count = 0
 
 
 def greeting
@@ -20,7 +23,6 @@ end
 
 
 
-
 def choose_character
     
     characters = []
@@ -34,17 +36,23 @@ def choose_character
         # binding.pry
         @current_character = characters[0][0]
         first = characters[0][0].name
-        puts "You Have Chosen #{first}"
+        @life_count = characters[0][0].lives
+        puts "You Have Chosen #{first}. You have #{@life_count} lives"
+
+
         
     elsif user_input == "2"
         @current_character = characters[0][1]
         second = characters[0][1].name
-        puts "You Have Chosen #{second}"
+        @life_count = characters[0][1].lives
+        puts "You Have Chosen #{second}.  You have #{@life_count} lives"
+
         
     elsif user_input == "3"
         @current_character = characters[0][2]
         third = characters[0][2].name
-        puts "You Have Chosen #{third}"
+        @life_count = characters[0][2].lives
+        puts "You Have Chosen #{third}  You have #{@life_count} lives"
         
         
         
@@ -58,71 +66,86 @@ def choose_character
 end
 
 
-def level1
-    level_array =[]
-    level_array << Level.all
-    puts "You Have Entered Phase 1 #{level_array[0][0].description}"
-    # fight 
-    # binding.pry
+# def level1
+#     level_array =[]
+#     level_array << Level.all
+#     puts "You Have Entered Phase 1 #{level_array[0][0].description}"
+#     # fight 
+#     # binding.pry
     
-end
+# end
+# def level2 
+#     level_array =[]
+#     level_array << Level.all
+#     puts "You Have Entered Phase 1 #{level_array[0][1].description}"
+#     fight 
 
-def fight
-    # name an attack(string) whenever that string is selected 
+# end 
+# def fight
+#     # name an attack(string) whenever that string is selected 
+#     @current_character.health = 100 
+#     characters = Character.all
+#     enemies = Enemy.all
+#     current_enemy = enemies[1]
+#     puts "1.Attack, 2. Dodge, 3. Heal"
     
-    characters = Character.all
-    enemies = Enemy.all
-    current_enemy = enemies[1]
-    puts "1.Attack, 2. Dodge, 3. Heal"
-    
-    # does a reduction on the enemy
-    while @current_character.health > 0 && current_enemy.health > 0
-        user_input = gets.chomp
+#     # does a reduction on the enemy
+#     while @current_character.health > 0 && current_enemy.health > 0
+#         user_input = gets.chomp
         
-        puts "1.Attack, 2. Dodge, 3. Heal"
-    
-    
-    if user_input == "1"
-        puts "#{@current_character.name} attacks #{current_enemy.name}"
-        # binding.pry
-        current_enemy.health -=  rand(1..10)
-        puts "#{current_enemy.name} health is at #{current_enemy.health}."
-
-        puts "#{current_enemy.name} attacks #{@current_character.name} back"
-        @current_character.health -= rand(1..10)
-        puts "Your health is at #{@current_character.health}."
-    elsif user_input == "2"
         
-        dodge_chance = rand(1..10)
-        if dodge_chance >= 5 
-        puts "#{current_enemy.name} attacks #{@current_character.name} but missed."
-        else
-            puts "#{@current_character.name} attempted to dodge but got hit."
-            @current_character.health -= 5
-            puts "Your health is at #{@current_character.health}."
+    
+    
+#         if user_input == "1"
+#             puts "#{@current_character.name} attacks #{current_enemy.name}"
+#             # binding.pry
+#             current_enemy.health -=  rand(1..10)
+#             puts "#{current_enemy.name} health is at #{current_enemy.health}."
 
-        end
-    elsif user_input == "3"
-        if @current_character.health > 50 
-            puts "Heal Unavailable..."
-        else
-            puts "You have healed.."
-            @current_character.health += rand(10..45)
-            puts "Your health is at #{@current_character.health}."
-        end
+#             puts "#{current_enemy.name} attacks #{@current_character.name} back"
+#             @current_character.health -= rand(1..10)
+#             puts "Your health is at #{@current_character.health}."
+#             puts "1.Attack, 2. Dodge, 3. Heal"
+#         elsif user_input == "2"
+            
+#             dodge_chance = rand(1..10)
+#             if dodge_chance >= 5 
+#             puts "#{current_enemy.name} attacks #{@current_character.name} but missed."
+            
+#             else
+#                 puts "#{@current_character.name} attempted to dodge but got hit."
+#                 @current_character.health -= 5
+#                 puts "Your health is at #{@current_character.health}."
 
-    end
+#             end
+#             puts "1.Attack, 2. Dodge, 3. Heal"
+#         elsif user_input == "3"
+#             if @current_character.health > 50 
+#                 puts "Heal Unavailable..."
+#             else
+#                 puts "You have healed.."
+#                 @current_character.health += rand(10..45)
+#                 puts "Your health is at #{@current_character.health}."
+#             end
+#             puts "1.Attack, 2. Dodge, 3. Heal"
+#         end
     
-end
+#     end
     
-    
-end
+#     if current_enemy.health <= 0
+#         puts "Congrats you beat #{current_enemy.name} Move on to next level."
+#     elsif @current_character.health <=0
+#         puts "#{current_enemy.name} proved too much for you...try again another day"
+#     end 
+        
+# end
 
 def run
     greeting
     choose_character
     level1  
     fight
+    level2
 end
 
 run
